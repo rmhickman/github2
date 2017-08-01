6bc71366410d8240c3044f859466206a6d2c55b0
Ronnie Hickman <rhickman2305@gmail.com>
2017-07-31 19:04:20 -0500

    added test files

---
+++ main.js
@@ -0,0 +1,7 @@
+var cat= "fluffy";
+console.log("inside main.js", cat);
+console.log("fixed problem");
+
+var catzContainer = document.getElementById('catz')
+console.log(catzContainer);
+catzContainer.innerHTML = cat;
